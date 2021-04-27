911fe7fdbe6ef27457e4dcd369d02844b120b325
RedGuy12 <paul@reid-family.org>
2021-04-27 11:26:49 -0500

    updates

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function sortList(list) {
 	switching = true;
 	while (switching) {
 		switching = false;
-		b = list.getElementsByTagName("li");
+		b         = list.getElementsByTagName("li");
 		for (i = 0; i < b.length - 1; i++) {
 			shouldSwitch = false;
-			if (
-				b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()
+			if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()
 			) {
 				shouldSwitch = true;
 				break;
